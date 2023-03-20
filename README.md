024c5bf2075d411ac93cab41e99cd76df795bd8b
Arnold Domingo <arnold.domingo@gmail.com>
2023-03-20 02:22:40 -0700

    changing the form submit to a pulldown instead of an input text field

--- README.md
+++ README.md
@@ -2,17 +2,20 @@ This is my solution to the Engineering Challenge located here:
 
 https://github.com/peck/engineering-assessment
 
-This code is actually live here:
+This code is running out of my house here:
 
-http://c-24-4-182-109.hsd1.ca.comcast.net/engineering-assessment.html
+http://c-24-4-182-109.hsd1.ca.comcast.net/cgi-bin/engineering-assessment.pl
 
-This now uses the csv endpoint provided in the assignment.
+This now uses the endpoint provided in the assignment.
 
 It is no longer using a local .csv file.
 
-If for whatever reason the link above doesn't work, it's simple to get running with these steps:
+If for whatever reason the link above doesn't work, it's simple to get running locally with these steps:
 
-	1.) Transfer the files to a webserver (note: I wrote this on a linux machine with apache2, but it should work anywhere a cgi script can run).
-	2.) Put 'engineering-assessment.pl' in a cgi directory.
-	3.) Put 'engineering-assessment.html' wherever the webroot is.
-	4.) If the cgi directory aliases to /cgi-bin/ in a browser, then this should already work.  If not then you either need to modify the <FORM> attribute in the html or modify the ScriptAlias directive in the apache configs (or whatever the equivalent of that is for your particular flavor of webserver)
+	1.) First, have a local webserver running under a VM or container, etc.
+	2.) Put 'engineering-assessment.pl' in the place where cgi scripts run.
+	3.) Go to http://<whatever-the-domain-is>/cgi-bin/engineering-assessment.pl
+
+NOTE: You may have to adjust the path of the $CSV_FILE variable to one that exists and is writable by the webserver. If you're running apache2 with Ubuntu and are using the stock ubuntu package repository then you most likely won't have to change this.
+
+This used to have a splash html page that is now deprecated because I changed it. I changed it because the list of food items to select from is now a pulldown menu that is now dynamically generated in the PERL script instead of being a free form text area in the static HTML.
--- cgi-bin/engineering-assessment.pl
+++ cgi-bin/engineering-assessment.pl
@@ -8,6 +8,27 @@ use CGI qw(:standard :cgi-lib);
 use strict;
 use warnings;
 
+sub get_food_options($) {
+	my $array_ref = shift();
+	my @aoh = @{$array_ref};
+	my %hash = ();
+	foreach my $data_ref (@aoh) {
+		my %data = %{$data_ref};
+		my $FoodItems = $data{'FoodItems'} || '';
+		if ($FoodItems) {
+			chomp($FoodItems);
+			my @items = split(/\s*[:;\&]+\s*/, $FoodItems);
+			foreach my $item (@items) {
+				next if (length($item) > 30);
+				my $lowercase = lc($item);
+				$hash{$lowercase} = 1;
+			}
+		}
+	}
+	my @array = keys(%hash);
+	return(@array);
+}
+
 sub get_remote_csv($$) {
 	my ($url, $file) = @_;
 	my $ua = LWP::UserAgent->new(timeout => 10);
@@ -26,12 +47,12 @@ sub get_remote_csv($$) {
 }
 
 sub get_favorites($$) {
-	my ($array_ref, $item) = @_;
+	my ($array_ref, $wanted) = @_;
 	my @all_trucks = @{$array_ref};
 	my @array = ();
 	foreach my $truck_ref (@all_trucks) {
 		my %truck_data = %{$truck_ref};
-		if ($truck_data{'FoodItems'} =~ m/$item/i) {
+		if ($truck_data{'FoodItems'} =~ m/$wanted/i) {
 			push(@array, $truck_ref);
 		}
 	}
@@ -43,37 +64,54 @@ sub get_favorites($$) {
 my $PARAMS = Vars();
 my $URL = 'https://data.sfgov.org/api/views/rqzj-sfat/rows.csv';
 my $FILENAME = basename($URL);
-my $CSV_FILE = '/tmp/' . $FILENAME;
+my $CSV_FILE = '/var/www/csv/' . $FILENAME;
 
 get_remote_csv($URL, $CSV_FILE);
 
 my $AOH = csv (in => "$CSV_FILE", headers => "auto");
-my $FOOD_ITEM = $$PARAMS{'food_item'};
-my @TRUCK_LIST = get_favorites($AOH, $FOOD_ITEM);
 
 print header();
 print start_html('Engineering Challenge');
-if (@TRUCK_LIST) {
-	print "<P>Alright, I have the following list of places that serve '$FOOD_ITEM':</P>\n";
-	print '<TABLE BORDER=1>' . "\n";
-	print '<TR>' . "\n";
-	print '<TH>Applicant</TH>' . "\n";
-	print '<TH>FoodItems</TH>' . "\n";
-	print '<TH>dayshours</TH>' . "\n";
-	print '<TH>Address</TH>' . "\n";
-	print '<TH>FacilityType</TH>' . "\n";
-	print '</TR>' . "\n";
-	foreach my $truck_data (@TRUCK_LIST) {
+print '<H1>Food Truck Finder</H1>' . "\n";
+print '<HR NOSHADE />' . "\n";
+if (exists($$PARAMS{'food_wanted'})) {
+	my $FOOD_WANTED = $$PARAMS{'food_wanted'};
+	my @TRUCK_LIST = get_favorites($AOH, $FOOD_WANTED);
+	if (@TRUCK_LIST) {
+		print "<P>Alright, I have the following list of places that serve '$FOOD_WANTED':</P>\n";
+		print '<TABLE BORDER=1>' . "\n";
 		print '<TR>' . "\n";
-		print "<TD>$$truck_data{'Applicant'}</TD>\n";
-		print "<TD>$$truck_data{'FoodItems'}</TD>\n";
-		print "<TD>$$truck_data{'dayshours'}</TD>\n";
-		print "<TD>$$truck_data{'Address'}</TD>\n";
-		print "<TD>$$truck_data{'FacilityType'}</TD>\n";
+		print '<TH>Applicant</TH>' . "\n";
+		print '<TH>FoodItems</TH>' . "\n";
+		print '<TH>dayshours</TH>' . "\n";
+		print '<TH>Address</TH>' . "\n";
+		print '<TH>FacilityType</TH>' . "\n";
 		print '</TR>' . "\n";
+		foreach my $truck_data (@TRUCK_LIST) {
+			print '<TR>' . "\n";
+			print "<TD>$$truck_data{'Applicant'}</TD>\n";
+			print "<TD>$$truck_data{'FoodItems'}</TD>\n";
+			print "<TD>$$truck_data{'dayshours'}</TD>\n";
+			print "<TD>$$truck_data{'Address'}</TD>\n";
+			print "<TD>$$truck_data{'FacilityType'}</TD>\n";
+			print '</TR>' . "\n";
+		}
+		print '</TABLE>' . "\n";
+	} else {
+		print "<P>Sorry, I couldn't find a list of food trucks that serve '$FOOD_WANTED'.</P>\n";
 	}
-	print '</TABLE>' . "\n";
 } else {
-	print "<P>Sorry, I couldn't find a list of food trucks that serve '$FOOD_ITEM'.</P>\n";
+	my @FOOD_OPTIONS = get_food_options($AOH);
+	print '<P>Welcome to the Food Truck Finder.' . "\n";
+	print 'Please select an item to find food trucks and carts in San Francisco that sell what you are hungry for.</P>' . "\n";
+	print '<FORM ACTION="/cgi-bin/engineering-assessment.pl" METHOD="get">' . "\n";
+	print "<LABEL FOR='food_wanted'>Food Item:</LABEL>\n";
+	print '<SELECT NAME="food_wanted" ID="food_wanted">' . "\n";
+	foreach my $option (@FOOD_OPTIONS) {
+		print "\t<OPTION VALUE='$option'>$option</OPTION>\n";
+	}
+	print '</SELECT>' . "\n";
+	print "<INPUT TYPE='submit' value='Submit'>\n";
+	print '</FORM>' . "\n";
 }
 print end_html();
