90eff059bf31fc68a19964b38ab5538e0d70857d
Arnold Domingo <arnold.domingo@gmail.com>
2023-03-20 14:01:56 -0700

    moving some html generating code into a couple of functions

--- cgi-bin/engineering-assessment.pl
+++ cgi-bin/engineering-assessment.pl
@@ -59,6 +59,46 @@ sub get_favorites($$) {
 	return(@array);
 }
 
+sub display_found_trucks(\@$) {
+	my ($array_ref, $food_wanted) = @_;
+	my @truck_list = @{$array_ref};
+		print "<P>Alright, I have the following list of places that serve '$food_wanted':</P>\n";
+		print '<TABLE BORDER=1>' . "\n";
+		print '<TR>' . "\n";
+		print '<TH>Applicant</TH>' . "\n";
+		print '<TH>FoodItems</TH>' . "\n";
+		print '<TH>dayshours</TH>' . "\n";
+		print '<TH>Address</TH>' . "\n";
+		print '<TH>FacilityType</TH>' . "\n";
+		print '</TR>' . "\n";
+		foreach my $truck_data (@truck_list) {
+			print '<TR>' . "\n";
+			print "<TD>$$truck_data{'Applicant'}</TD>\n";
+			print "<TD>$$truck_data{'FoodItems'}</TD>\n";
+			print "<TD>$$truck_data{'dayshours'}</TD>\n";
+			print "<TD>$$truck_data{'Address'}</TD>\n";
+			print "<TD>$$truck_data{'FacilityType'}</TD>\n";
+			print '</TR>' . "\n";
+		}
+		print '</TABLE>' . "\n";
+}
+
+sub display_spash_page(\@) {
+	my $array_ref = shift();
+	my @food_options = @{$array_ref};
+	print '<P>Welcome to the Food Truck Finder.' . "\n";
+	print 'Please select an item to find food trucks and carts in San Francisco that sell what you are hungry for.</P>' . "\n";
+	print '<FORM ACTION="/cgi-bin/engineering-assessment.pl" METHOD="get">' . "\n";
+	print "<LABEL FOR='food_wanted'>Food Item:</LABEL>\n";
+	print '<SELECT NAME="food_wanted" ID="food_wanted">' . "\n";
+	foreach my $option (@food_options) {
+		print "\t<OPTION VALUE='$option'>$option</OPTION>\n";
+	}
+	print '</SELECT>' . "\n";
+	print "<INPUT TYPE='submit' value='Submit'>\n";
+	print '</FORM>' . "\n";
+}
+
 ## Main
 
 my $PARAMS = Vars();
@@ -76,42 +116,14 @@ print '<H1>Food Truck Finder</H1>' . "\n";
 print '<HR NOSHADE />' . "\n";
 if (exists($$PARAMS{'food_wanted'})) {
 	my $FOOD_WANTED = $$PARAMS{'food_wanted'};
-	my @TRUCK_LIST = get_favorites($AOH, $FOOD_WANTED);
-	if (@TRUCK_LIST) {
-		print "<P>Alright, I have the following list of places that serve '$FOOD_WANTED':</P>\n";
-		print '<TABLE BORDER=1>' . "\n";
-		print '<TR>' . "\n";
-		print '<TH>Applicant</TH>' . "\n";
-		print '<TH>FoodItems</TH>' . "\n";
-		print '<TH>dayshours</TH>' . "\n";
-		print '<TH>Address</TH>' . "\n";
-		print '<TH>FacilityType</TH>' . "\n";
-		print '</TR>' . "\n";
-		foreach my $truck_data (@TRUCK_LIST) {
-			print '<TR>' . "\n";
-			print "<TD>$$truck_data{'Applicant'}</TD>\n";
-			print "<TD>$$truck_data{'FoodItems'}</TD>\n";
-			print "<TD>$$truck_data{'dayshours'}</TD>\n";
-			print "<TD>$$truck_data{'Address'}</TD>\n";
-			print "<TD>$$truck_data{'FacilityType'}</TD>\n";
-			print '</TR>' . "\n";
-		}
-		print '</TABLE>' . "\n";
+	my @FOUND_LIST = get_favorites($AOH, $FOOD_WANTED);
+	if (@FOUND_LIST) {
+		display_found_trucks(@FOUND_LIST, $FOOD_WANTED);
 	} else {
 		print "<P>Sorry, I couldn't find a list of food trucks that serve '$FOOD_WANTED'.</P>\n";
 	}
 } else {
 	my @FOOD_OPTIONS = get_food_options($AOH);
-	print '<P>Welcome to the Food Truck Finder.' . "\n";
-	print 'Please select an item to find food trucks and carts in San Francisco that sell what you are hungry for.</P>' . "\n";
-	print '<FORM ACTION="/cgi-bin/engineering-assessment.pl" METHOD="get">' . "\n";
-	print "<LABEL FOR='food_wanted'>Food Item:</LABEL>\n";
-	print '<SELECT NAME="food_wanted" ID="food_wanted">' . "\n";
-	foreach my $option (@FOOD_OPTIONS) {
-		print "\t<OPTION VALUE='$option'>$option</OPTION>\n";
-	}
-	print '</SELECT>' . "\n";
-	print "<INPUT TYPE='submit' value='Submit'>\n";
-	print '</FORM>' . "\n";
+	display_spash_page(@FOOD_OPTIONS);
 }
 print end_html();
